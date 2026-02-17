11d0c920aad56cc86f99d58a19363d8578d68c27
Kevin Luxa <k.luxa@unsw.edu.au>
2026-02-18 00:35:19 +1100

    configured static export for production builds

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from 'next';
+import type { PHASE_TYPE } from 'next/constants';
+import { PHASE_PRODUCTION_BUILD } from 'next/constants';
 
-const nextConfig: NextConfig = {
+const nextConfig = (
+	phase: PHASE_TYPE,
+	{ defaultConfig }: { defaultConfig: NextConfig },
+): NextConfig => {
 	/* config options here */
+	if (phase === PHASE_PRODUCTION_BUILD) {
+		return {
+			...defaultConfig,
+			output: 'export',
+			trailingSlash: true,
+		};
+	}
+
+	return defaultConfig;
 };
 
 export default nextConfig;
