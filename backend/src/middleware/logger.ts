2a0108402c66ca45f59ecccdcbd667cfe99ed546
Kevin Luxa <k.luxa@unsw.edu.au>
2026-02-24 11:13:41 +1100

    changed format of log messages and improved logger code

--- backend/src/middleware/logger.ts
+++ backend/src/middleware/logger.ts
@@ -1,8 +1,11 @@
 import fs from 'fs';
 import path from 'path';
 
+import { format } from 'date-fns';
 import type { Request } from 'express';
 import morgan from 'morgan';
+
+import { toLocalDate } from '@/lib/date';
 
 const logStream = fs.createWriteStream(
 	path.join('instance', 'logs', 'requests.log'),
@@ -11,12 +14,17 @@ const logStream = fs.createWriteStream(
 	},
 );
 
+morgan.token('date', () => {
+	const date = toLocalDate(new Date());
+	return format(date, 'yyyy-MM-dd hh:mm:ss');
+});
+
 morgan.token('user', (req: Request) =>
 	req.maybeUser ? req.maybeUser.zid : 'anonymous',
 );
 
 const logger = morgan(
-	'[:date[iso]] :remote-addr :user ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent"',
+	'[:date] :remote-addr :user ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent"',
 	{ stream: logStream },
 );
 
