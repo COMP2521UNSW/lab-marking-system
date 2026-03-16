68ae32b7a242a9b9d31ab6ba4393fc624195a2a9
Kevin Luxa <k.luxa@unsw.edu.au>
2026-03-16 19:42:36 +1100

    changed access log output stream to stdout

--- backend/src/middleware/logger.ts
+++ backend/src/middleware/logger.ts
@@ -1,19 +1,8 @@
-import fs from 'fs';
-import path from 'path';
-
 import { format } from 'date-fns';
 import type { Request } from 'express';
 import morgan from 'morgan';
 
 import { toLocalDate } from '@/lib/date';
-import { rootDir } from '@@/path-config';
-
-const logStream = fs.createWriteStream(
-	path.join(rootDir, 'logs', 'access.log'),
-	{
-		flags: 'a',
-	},
-);
 
 morgan.token('date', () => {
 	const date = toLocalDate(new Date());
@@ -26,7 +15,6 @@ morgan.token('user', (req: Request) =>
 
 const logger = morgan(
 	'[:date] :remote-addr :user ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent"',
-	{ stream: logStream },
 );
 
 export { logger };
